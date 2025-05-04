a4fd45a7ae228abf27a6e037c787d4ba778a9284
Kenny Edgar <kenny.edgar.21@gmail.com>
2025-05-04 13:06:31 -0700

    Clean rebuild of EveCore

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
-
+from flask import Flask, render_template, request, jsonify
 import json
 import os
 
+app = Flask(__name__)
 MEMORY_FILE = 'memory.json'
 
 def load_memory():
@@ -16,21 +15,26 @@ def save_memory(data):
     with open(MEMORY_FILE, 'w') as f:
         json.dump(data, f, indent=2)
 
+@app.route("/", methods=["GET", "POST"])
+def index():
+    memory = load_memory()
+    if request.method == "POST":
+        entry = request.form.get("entry", "")
+        if entry:
+            memory.append(entry)
+            save_memory(memory)
+    return render_template("index.html", memory=memory)
 
-from flask import request, jsonify
-
-@app.route('/memory', methods=['GET', 'POST'])
+@app.route("/memory", methods=["GET", "POST"])
 def memory():
-    if request.method == 'GET':
+    if request.method == "GET":
         return jsonify(load_memory())
-
-    elif request.method == 'POST':
+    elif request.method == "POST":
         new_entry = request.json.get('entry')
         memory = load_memory()
         memory.append(new_entry)
         save_memory(memory)
         return jsonify({'status': 'success', 'entry_added': new_entry})
-
 
 if __name__ == "__main__":
     app.run()
